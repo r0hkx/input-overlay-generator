18a504ce58b0347a9599a8988cc02723fb6efe04
r0hkx <67075923+r0hkx@users.noreply.github.com>
2024-10-22 11:12:31 -0700

    uploaded example_layout.py and r0hkx_layout.py

---
+++ example_layout.py
@@ -0,0 +1,25 @@
+from generator import add_rectangle, add_image, generate_overlay_files
+
+# hotbar
+add_rectangle(key="1", pos=(696, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100))
+add_rectangle(key="2", pos=(756, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100))
+add_rectangle(key="3", pos=(816, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100))
+add_rectangle(key="4", pos=(876, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100))
+add_rectangle(key="X", pos=(936, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100))
+add_rectangle(key="V", pos=(996, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100))
+add_rectangle(key="F", pos=(1056, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100))
+add_rectangle(key="Z", pos=(1116, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100))
+add_rectangle(key="C", pos=(1176, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100))
+add_rectangle(key="K", pos=(609, 1023), size=(48, 48), pressed_color=(255, 255, 255, 100), not_pressed_color=(100, 0, 100, 255))
+
+# actionbar
+add_image(key="W", pos=(801, 792), pressed_image="images/W.png", not_pressed_image="images/E.png")
+add_image(key="A", pos=(771, 828), pressed_image="images/A.png")
+add_image(key="S", pos=(801, 828), pressed_image="images/S.png")
+add_image(key="D", pos=(831, 828), pressed_image="images/D.png")
+add_image(key="SPACE", pos=(765, 864), pressed_image="images/Space.png")
+add_image(key="MOUSE_BUTTON1", pos=(1050, 864), pressed_image="images/LMB.png")
+add_image(key="MOUSE_BUTTON2", pos=(1116, 864), pressed_image="images/RMB.png")
+add_image(key="MOUSE_BUTTON3", pos=(1036, 828), pressed_image="images/Pick Block.png")
+
+generate_overlay_files((1920, 1080))
---
+++ r0hkx_layout.py
@@ -0,0 +1,50 @@
+from generator import add_rectangle, add_image, generate_overlay_files
+
+def c(key):
+    key_conversion = {
+        "CAPS_LOCK": "BACKSPACE",
+        "A": "Z",
+        "D": "X",
+        "Z": "A",
+        "X": "D",
+    }
+    if key not in key_conversion:
+        return key
+    else:
+        return key_conversion[key]
+
+# hotbar
+hotbar_color = (255, 255, 255, 100)
+hotbar_x_coord = 696
+hotbar_keys = ["1", "2", "3", "4", "X", "V", "F", "Z", "C"]
+for x in range(9):
+    add_rectangle(key=c(hotbar_keys[x]), pos=(hotbar_x_coord, 1023), size=(48, 48), pressed_color=hotbar_color)
+    hotbar_x_coord += 60
+
+# offhand
+add_rectangle(key="K", pos=(609, 1023), size=(48, 48), pressed_color=hotbar_color)
+
+# left actionbar stuff
+row1 = 756
+row2 = 792
+row3 = 828
+row4 = 864
+add_image(key=c("BACKQUOTE"), pos=(657, row1), pressed_image="images/Sprint.png")
+add_image(key=c("ESCAPE"), pos=(693, row2), pressed_image="images/Esc.png")
+add_image(key=c("TAB"), pos=(753, row2), pressed_image="images/F5.png")
+add_image(key=c("W"), pos=(801, row2), pressed_image="images/W.png")
+add_image(key=c("E"), pos=(831, row2), pressed_image="images/E.png")
+add_image(key=c("F3"), pos=(861, row2), pressed_image="images/F3.png")
+add_image(key=c("CAPS_LOCK"), pos=(681, row3), pressed_image="images/Chat.png")
+add_image(key=c("A"), pos=(771, row3), pressed_image="images/A.png")
+add_image(key=c("S"), pos=(801, row3), pressed_image="images/S.png")
+add_image(key=c("D"), pos=(831, row3), pressed_image="images/D.png")
+add_image(key=c("SHIFT_L"), pos=(678, row4), pressed_image="images/Shift.png")
+add_image(key=c("SPACE"), pos=(765, row4), pressed_image="images/Space.png")
+
+# right actionbar stuff
+add_image(key="MOUSE_BUTTON1", pos=(1050, row4), pressed_image="images/LMB.png")
+add_image(key="MOUSE_BUTTON2", pos=(1116, row4), pressed_image="images/RMB.png")
+add_image(key="MOUSE_BUTTON3", pos=(1036, row3), pressed_image="images/Pick Block.png")
+
+generate_overlay_files((1920, 1080))
