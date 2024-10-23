6b2dcbcaeb743d2e614e975197db2780e4dac956
r0hkx <67075923+r0hkx@users.noreply.github.com>
2024-10-22 21:53:01 -0700

    Update r0hkx_layout.py

--- r0hkx_layout.py
+++ r0hkx_layout.py
@@ -25,13 +25,15 @@ for x in range(9):
 add_rectangle(key="K", pos=(609, 1023), size=(48, 48), pressed_color=hotbar_color)
 
 # left actionbar stuff
+row0 = 720
 row1 = 756
 row2 = 792
 row3 = 828
 row4 = 864
-add_image(key=c("BACKQUOTE"), pos=(657, row1), pressed_image="images/Sprint.png")
-add_image(key=c("ESCAPE"), pos=(693, row2), pressed_image="images/Esc.png")
-add_image(key=c("TAB"), pos=(753, row2), pressed_image="images/F5.png")
+add_image(key=c("BACKQUOTE"), pos=(669, row0), pressed_image="images/Sprint.png")
+add_image(key=c("ESCAPE"), pos=(705, row1), pressed_image="images/Esc.png")
+add_image(key=c("TAB"), pos=(723, row2), pressed_image="images/F5.png")
+add_image(key=c("Q"), pos=(771, row2), pressed_image="images/Q.png")
 add_image(key=c("W"), pos=(801, row2), pressed_image="images/W.png")
 add_image(key=c("E"), pos=(831, row2), pressed_image="images/E.png")
 add_image(key=c("F3"), pos=(861, row2), pressed_image="images/F3.png")
